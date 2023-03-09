8ae17bf9c7056a92e6de950c3353142732d99c0d
George Wanderson <georgewanderson07@gmail.com>
2023-03-09 16:22:59 -0300

    Implemented Controller Methods

--- ToDoListWebAPI/Controllers/UserController.cs
+++ ToDoListWebAPI/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ToDoListWebAPI.Models;
+using ToDoListWebAPI.Repositories.Interfaces;
 
 namespace ToDoListWebAPI.Controllers
 {
@@ -8,10 +9,47 @@ namespace ToDoListWebAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<UserModel>> GetAllUsers()
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
         {
-            return Ok();
+             _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserModel>>> GetAllUsers()
+        {
+            List<UserModel> users = await _userRepository.GetAllUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserModel>> GetById(int id)
+        {
+           UserModel user = await _userRepository.GetById(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserModel>> Add([FromBody] UserModel userModel)
+        {
+            UserModel user = await _userRepository.Add(userModel);
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserModel>> Update([FromBody] UserModel userModel, int id)
+        {
+            userModel.Id = id;
+            UserModel user = await _userRepository.Update(userModel, id);
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UserModel>> DeleteById(int id)
+        {
+            bool deleted =  await _userRepository.DeleteById(id);
+            return Ok(deleted);
         }
 
 
